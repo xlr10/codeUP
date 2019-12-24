1b3491194b7706fdee1ebc81572c048efa00a5b5
xlr10 <vjihovjiho@gmail.com>
2019-12-24 23:16:13 +0900

    codeUP_100 191224 (20/100)

--- basic100_191224/src/basic_1019.java
+++ basic100_191224/src/basic_1019.java
@@ -5,6 +5,10 @@ public class basic_1019 {
         Scanner scanner=new Scanner(System.in);
         String[] tmp=scanner.nextLine().split("\\.");
 
+        for(String e: tmp){
+            System.out.println(e);
+        }
+
         System.out.format("%04d.%02d.%02d",
                 Integer.parseInt(tmp[0]),
                 Integer.parseInt(tmp[1]),
